8e228b3395bd08bbfaeabe2ce0b97d08dcbf9b83
icehopeless <gabrielagostinhodasilva@hotmail.com>
2023-07-25 00:09:50 -0300

    Flappy Bird

--- src/Flappy.hpp
+++ src/Flappy.hpp
@@ -9,17 +9,17 @@
 using namespace std;
 
 class FlappyBird{
-    float frame, space, gravity;
-    int count;
-    shared_ptr<sf::RenderWindow> window; //ponteiro inteligente para ser deletado automaticamente
-    shared_ptr<sf::Sprite> bird, pipetop, pipeback, background,restart, quit; // ponteiros inteligentes para fazerem alocações dinâmicas em  Sprites
-    sf::Texture bird_Texture[3], pipe,bg,restart_texture,quit_texture;  
-    vector<sf::Sprite> pipes; //vetor inteligente para guardar os pipes e apaga-los
-    int updateTexture;
-    bool mouseup, GameOver;
-    sf::Font font;
-    sf::Text gmText;
-   	sf::Vector2i pos_mouse;
+   float frame, space, gravity;
+   int count;
+   shared_ptr<sf::RenderWindow> window; //ponteiro inteligente para ser deletado automaticamente
+   shared_ptr<sf::Sprite> bird, pipetop, pipeback, background,restart, quit; // ponteiros inteligentes para fazerem alocações dinâmicas em  Sprites
+   sf::Texture bird_Texture[3], pipe,bg,restart_texture,quit_texture;  
+   vector<sf::Sprite> pipes; //vetor inteligente para guardar os pipes e apaga-los
+   int updateTexture;
+   bool mouseup, GameOver;
+   sf::Font font;
+   sf::Text gmText;
+   sf::Vector2i pos_mouse;
 	sf::Vector2f mouse_coord;
    
    protected:
